55a28add1ca49023087fd82d82a669429b7dc355
nathanmotta <142407439+nathanmotta@users.noreply.github.com>
2023-09-08 20:50:22 -0300

    Add files via upload

---
+++ src/Tabuada.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int contador = 1, result = 0;
+        System.out.println("Entre com o número tabuada: ");
+        int nt = x.nextInt();
+
+        while (contador <= 10) {
+            result = nt * contador;
+            System.out.println(nt + " X " + contador + " = " + result);
+            contador++;
+        }
+    }
+}
